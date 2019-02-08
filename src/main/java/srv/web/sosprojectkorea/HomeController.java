24221099fa1d1f9320256711561d08548006103c
fleamon <fleamon@nate.com>
2019-02-08 23:08:26 +0900

    test commit

---
+++ src/main/java/srv/web/sosprojectkorea/HomeController.java
@@ -0,0 +1,71 @@
+package srv.web.sosprojectkorea;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate);
+
+		return "home";
+	}
+
+	@RequestMapping(value = "/jsonData", method = RequestMethod.GET)
+	public String jsonData(Locale jsonData, Model model) {
+		JSONObject obj = new JSONObject();
+		obj.put("name", "mkyong.com");
+		obj.put("age", new Integer(100));
+		// 123
+		JSONArray list = new JSONArray();
+		list.add("msg 1");
+		list.add("msg 2");
+		list.add("msg 3");
+
+		obj.put("messages", list);
+
+		try {
+
+			FileWriter file = new FileWriter("c:\\test.json");
+			file.write(obj.toJSONString());
+			file.flush();
+			file.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.print(obj);
+
+		return "jsonData";
+	}
+}
